81f0579f34a428794d83137cbea19a33a075ed30
Sebastian@XIDV <sebastian.peschl@xidv.de>
2022-10-09 18:05:40 +0200

    ersten Content hinzugefügt bla

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', dcl => {
     console.log('Document is ready ...');
+
+    // document.querySelector('#subBspl1Data').addEventListener('click', e => {
+    //     e.preventDefault();
+    //     console.log(e);
+    // });
+
+    document.querySelector('#bspl1').addEventListener('submit', e => {
+        e.preventDefault();
+        const inputs = e.target.querySelectorAll('input');
+        inputs.forEach(input => {
+            console.log(input.value);
+        });
+        showOutputDialog(inputs, document.querySelector('#bspl1Output'));
+    });
+
 });
+
+
+function showOutputDialog(inputs, dialog) {
+    dialog.show();
+
+    console.log(dialog);
+
+}
