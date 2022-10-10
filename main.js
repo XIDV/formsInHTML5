6ded22244d9021d365ec54b72018598a302f3ee3
Sebastian@XIDV <sebastian.peschl@xidv.de>
2022-10-10 15:11:41 +0200

    Die Eingabefelder date, color und range eingefügt und erleutert. bei range wird value abgegriffen und in einem ausgabefeld angezeigt

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', dcl => {
     console.log('Document is ready ...');
 
+
+    document.querySelector('#satisfaction').addEventListener('input', e => {
+        document.querySelector('#satOutput').value = e.target.value;
+    });
 });
